ba6dd2e6c5dfd6f934390760be15c6714b3a98d5
dimahelper <dimahelper@mail.ru>
2019-10-09 14:50:39 +0300

    "Тест создания и подписания Сообщения о начале расчетов"

--- src/test/java/ru/qa/test/bankrot/tests/MessageSignTest4.java
+++ src/test/java/ru/qa/test/bankrot/tests/MessageSignTest4.java
@@ -47,6 +47,68 @@ public class MessageSignTest4 extends TestBase {
     app.getSignMessage().signMessage();
   }
 
+  @Test(priority = 46)
+  @Description("Тест создания и подписания Сведений о смете текущих расходов кредитной организации или иной финансовой организации")
+  public void testEstimatesCurrentExpenses() throws InterruptedException {
+    app.getMessagesListPage().clickAddMessage();
+    app.getNewMessagePage().selectMessageAndGoNext(12, 4);
+    app.getCreateMessage().fillBasicData("Сведения о смете текущих расходов кредитной организации или иной финансовой организации", "none");
+    app.getCreateMessage().clickSignMessage();
+    app.getSignMessage().signMessage();
+  }
+
+  @Test(priority = 47)
+  @Description("Тест создания и подписания Сведений о скорректированной смете текущих расходов кредитной организации или иной финансовой организации")
+  public void testChangeEstimatesCurrentExpenses() throws InterruptedException {
+    app.getMessagesListPage().clickAddMessage();
+    app.getNewMessagePage().selectMessageAndGoNext(12, 5);
+    app.getCreateMessage().fillBasicData("Сведения о скорректированной смете текущих расходов кредитной организации или иной финансовой организации", "none");
+    app.getCreateMessage().selectMessageFromTheList("для ссылки на сведения о корректируемой смете");
+    app.getCreateMessage().clickSignMessage();
+    app.getSignMessage().signMessage();
+  }
+
+  @Test(priority = 48)
+  @Description("Тест создания и подписания Сведений о порядке и сроках расчетов с кредиторами")
+  public void testOrderAndTimingCalculations() throws InterruptedException {
+    app.getMessagesListPage().clickAddMessage();
+    app.getNewMessagePage().selectMessageAndGoNext(12, 6);
+    app.getCreateMessage().fillBasicData("Сведения о порядке и сроках расчетов с кредиторами", "none");
+    app.getCreateMessage().clickSignMessage();
+    app.getSignMessage().signMessage();
+  }
+
+  @Test(priority = 49)
+  @Description("Тест создания и подписания Информации о ходе конкурсного производства")
+  public void testInformationAboutBankruptcy() throws InterruptedException {
+    app.getMessagesListPage().clickAddMessage();
+    app.getNewMessagePage().selectMessageAndGoNext(12, 7);
+    app.getCreateMessage().fillBasicData("Информация о ходе конкурсного производства", "none");
+    app.getCreateMessage().clickSignMessage();
+    app.getSignMessage().signMessage();
+  }
+
+  @Test(priority = 50)
+  @Description("Тест создания и подписания Сведений об исполнении сметы текущих расходов и стоимости нереализованного имущества кредитной организации")
+  public void testEstimatesAndUnsoldAssets() throws InterruptedException {
+    app.getMessagesListPage().clickAddMessage();
+    app.getNewMessagePage().selectMessageAndGoNext(12, 8);
+    app.getCreateMessage().fillBasicData("Сведения об исполнении сметы текущих расходов и стоимости нереализованного имущества кредитной организации", "none");
+    app.getCreateMessage().clickSignMessage();
+    app.getSignMessage().signMessage();
+  }
+
+  @Test(priority = 51)
+  @Description("Тест создания и подписания Сообщения о начале расчетов")
+  public void testStartSettlement() throws InterruptedException {
+    app.getMessagesListPage().clickAddMessage();
+    app.getNewMessagePage().selectMessageAndGoNext(12, 9);
+    app.getCreateMessage().fillBasicData("Сообщения о начале расчетов", app.getHelperBase().formCurDate);
+    app.getCreateMessage().clickSignMessage();
+    app.getSignMessage().signMessage();
+  }
+
+
 
 }
 
