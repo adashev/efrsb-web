ad91b351f509adaaa8dd41eb6bc30e3ff8d9aeac
adashev <dimahelper@mail.ru>
2019-11-12 11:27:43 +0300

    сокращен sleep

--- src/test/java/ru/qa/test/bankrot/tests/TestBase.java
+++ src/test/java/ru/qa/test/bankrot/tests/TestBase.java
@@ -14,7 +14,7 @@ public class TestBase {
   @BeforeTest(alwaysRun = true)
   @Parameters("user")
   public void setUp(@Optional("au") String user, ITestContext context) throws Exception {//, description = "Инициализация браузера и авторизация в АРМ"
-    Thread.sleep(6000);
+    Thread.sleep(1000);
     app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME), user);
     app.init();
   }
